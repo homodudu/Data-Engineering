7561ee89d8f3b4bd41f332015427a848ecf3483c
lullobytes <48366159+homodudu@users.noreply.github.com>
2022-10-16 13:14:08 +0100

    Updated python script

--- us_population.py
+++ us_population.py
@@ -15,7 +15,7 @@ for x in range(0, len(list)) :
     dict[x] = list[x]
 
 from pathlib import Path  
-filepath = Path('/Users/macbook/Documents/Github/Data-Engineering/us_population/us_population.csv')  
+filepath = Path('/Users/macbook/Documents/Github/Data-Engineering/us_population.csv')  
 filepath.parent.mkdir(parents=True, exist_ok=True)  
 pd.DataFrame.from_dict(data=dict,orient='index').to_csv(filepath, header=True)
 
