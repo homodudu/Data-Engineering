8276a6df8f5528490b2ebac8dd5b0e28a6188368
lullobytes <48366159+homodudu@users.noreply.github.com>
2023-04-19 13:49:38 +0100

    F1 data engineering exercise. Added pandas and pyspark scripts..

---
+++ _practice/3. Class Setup Practice.py
@@ -0,0 +1,20 @@
+# 1. Define Class for complex numbers
+import math
+class ZNumber():
+    def __init__(self, real:int, imag:int):
+        self.real = real
+        self.imag = imag
+
+    def mod(self):
+        return math.sqrt(self.real**2 + self.imag**2)
+
+    def arg(self):
+        return math.atan2(self.imag, self.real)
+
+# 2. Define program entry point.
+def main():
+    z = ZNumber(42,84)
+    print(z.mod(),z.arg())
+
+if __name__ == '__main__':
+    main()
